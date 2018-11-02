f368a14d860ef737d19dd6b196f651678be262b8
LindaBot <voidnullbot@outlook.com>
2018-11-02 17:58:15 +1300

    Read from light sensor

--- app/src/main/java/com/lindabot/solartorch/MainActivity.java
+++ app/src/main/java/com/lindabot/solartorch/MainActivity.java
@@ -4,19 +4,31 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     ToggleButton button;
+    private SensorManager mSensorManager;
+    private Sensor mAmbientLight;
+    float lightSensorValue = 0;
+    TextView textView;
 
 
     @Override
@@ -24,21 +36,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Context context = getApplicationContext();
-
+        textView = (TextView) findViewById(R.id.sensorValue);
         button = findViewById(R.id.torch);
 
+        // Torch part
         final CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
         boolean feature_camera_flash = getPackageManager().hasSystemFeature(getPackageManager().FEATURE_CAMERA_FLASH);
         boolean cameraAccess = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
         ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 60);
-
-
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
                     String cameraID = cameraManager.getCameraIdList()[0];
+                    Log.d("Button", "Button pressed");
                     if (button.isChecked()){
                         cameraManager.setTorchMode(cameraID, true);
                     }else{
@@ -51,7 +62,39 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // Light sensor part
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAmbientLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
     }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mSensorManager.registerListener(this, mAmbientLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event){
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT){
+            lightSensorValue = event.values[0];
+            String valueString = Float.toString(lightSensorValue);
+            textView.setText(valueString);
+            // updateString();
+        }
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy){
+        // Do nothing
+    }
+
 
 
 
